4844ff8aebe69db33178355fb783d419adee686f
eliezersamuel <eliezer_sss@hotmail.com>
2024-06-06 00:38:53 -0300

    feat: initialize

---
+++ README.md
@@ -0,0 +1,54 @@
+# typeOf
+
+A utility function to determine the type of a given argument and return an object containing the type and value.
+
+## Installation
+
+To install the package, run:
+
+```bash
+npm install type-of-utility
+```
+
+## Usage
+
+The typeOf function can be used to determine the type of various values. The function returns an object with the type and the value.
+
+```javascript
+import { typeOf } from "type-of-utility";
+
+console.log(typeOf(null)); // { type: "null", value: null }
+console.log(typeOf(123)); // { type: "number", value: 123 }
+console.log(typeOf("123")); // { type: "number", value: 123 }
+console.log(typeOf("hello")); // { type: "string", value: "hello" }
+console.log(typeOf([1, 2, 3])); // { type: "array", value: [1, 2, 3] }
+console.log(typeOf({ key: "value" })); // { type: "object", value: { key: "value" } }
+```
+
+## API
+
+typeOf(arg)
+Determines the type of a given argument and returns an object containing the type and value.
+
+Parameters
+
+-   arg (\*): The argument whose type is to be determined.
+
+Returns
+
+-   { type: string, value: \*}: An object containing the type and value of the argument.
+
+Example
+
+```javascript
+typeOf(null); // { type: "null", value: null }
+typeOf(123); // { type: "number", value: 123 }
+typeOf("123"); // { type: "number", value: 123 }
+typeOf("hello"); // { type: "string", value: "hello" }
+typeOf([1, 2, 3]); // { type: "array", value: [1, 2, 3] }
+typeOf({ key: "value" }); // { type: "object", value: { key: "value" } }
+```
+
+## License
+
+This project is licensed under the MIT License.
---
+++ index.js
@@ -0,0 +1,62 @@
+/**
+ * Determines if the given argument is an object or array and returns an object containing the type and value.
+ *
+ * @param {*} arg - The argument to check.
+ * @returns {{ type: string, value: * }} An object containing the type ("object" or "array") and value of the argument.
+ */
+function isObject(arg) {
+    let value = arg;
+    if (Array.isArray(arg)) return { type: "array", value };
+    return { type: "object", value };
+}
+
+/**
+ * Determines the type of a given argument and returns an object containing the type and value.
+ *
+ * The possible types are:
+ * - "null" for null values
+ * - "number" for numeric values or strings that can be converted to numbers
+ * - "string" for string values that cannot be converted to numbers or objects
+ * - "boolean" for boolean values
+ * - "symbol" for symbol values
+ * - "undefined" for undefined values
+ * - "function" for function values
+ * - "object" for objects that are not arrays
+ * - "array" for array values
+ *
+ * @param {*} arg - The argument whose type is to be determined.
+ * @returns {{type: string, value: *}} An object containing the type and value of the argument.
+ *
+ * @example
+ * typeOf(null); // { type: "null", value: null }
+ * typeOf(123); // { type: "number", value: 123 }
+ * typeOf("123"); // { type: "number", value: 123 }
+ * typeOf("hello"); // { type: "string", value: "hello" }
+ * typeOf([1, 2, 3]); // { type: "array", value: [1, 2, 3] }
+ * typeOf({ key: "value" }); // { type: "object", value: { key: "value" } }
+ */
+export function typeOf(arg) {
+    let value = arg;
+    const type = typeof arg;
+
+    if (arg === null || arg === "null") return { type: "null", value: null };
+
+    if (type === "object") {
+        return isObject(arg);
+    }
+
+    if (type === "string") {
+        if (!isNaN(arg)) {
+            value = +arg;
+            return { type: "number", value };
+        }
+
+        if (typeof JSON.parse(arg) === "object") {
+            return isObject(JSON.parse(arg));
+        }
+
+        return { type: "string", value };
+    }
+
+    return { type, value };
+}
